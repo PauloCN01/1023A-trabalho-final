6b91b01d7ab76be2963b5b75d541b6d6405a7911
PauloCN01 <paulo.ferreira3@estudante.ifms.edu.br>
2025-07-07 10:04:18 -0300

    input feitos(HTML)

--- src/CadastroVendedor.tsx
+++ src/CadastroVendedor.tsx
@@ -3,31 +3,31 @@ import './Cadastro.css'
 
 function PaginaCadastro() {
 
-    function TrataCadastro(){
+    function TrataCadastro() {
 
     }
-    return(
+    return (
         <>
-        <header>
+            <header>
 
-        </header>
-        <main>
-            <div className="container-cadastro">
-                <form onSubmit={TrataCadastro}>
-                <input type="text" name="nome" id="nome" />
-                <input type="text" name="cpf" id="cpf" />
-                <input type="text" name="endereco" id="endereco" />
-                <input type="email" name="email" id="email" />
-                <select name="genero" id="genero">
-                    <option value="M">Masculino</option>
-                    <option value="F">Feminino</option>
-                    <option value="Outro">Outro</option>
-                </select>
-                <input type="submit" value="Cadastrar" />
-                </form>
-            </div>
-        </main>
-        <footer></footer>
+            </header>
+            <main>
+                <div className="container-cadastro">
+                    <form onSubmit={TrataCadastro}>
+                        <input type="text" name="nome" id="nome" placeholder="Nome" />
+                        <input type="text" name="cpf" id="cpf" placeholder="CPF" />
+                        <input type="email" name="email" id="email" placeholder="Email" />
+                        <input type="password" name="senha" id="senha" placeholder="Senha" />
+                        <select name="genero" id="genero">
+                            <option value="M">Masculino</option>
+                            <option value="F">Feminino</option>
+                            <option value="Outro">Outro</option>
+                        </select>
+                        <input type="submit" value="Cadastrar" />
+                    </form>
+                </div>
+            </main>
+            <footer></footer>
         </>
     )
 }
